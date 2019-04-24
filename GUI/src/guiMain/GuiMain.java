6891c2b81e2e7612eadb090ebaff0b39fe01cddb
violetaplum <violetaplume@gmail.com>
2019-04-24 21:07:03 +0900

    gfd

---
+++ GUI/src/guiMain/GuiMain.java
@@ -0,0 +1,15 @@
+package guiMain;
+import java.awt.*;
+public class GuiMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Frame f1 = new Frame();
+		f1.setTitle("It's me");
+		f1.setSize(700,700);
+		f1.setVisible(true);
+		
+	}
+
+}
