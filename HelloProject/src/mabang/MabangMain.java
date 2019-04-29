7b5cc77dd951c56ec3235309ab9c182219fea83a
violetaplum <violetaplume@gmail.com>
2019-04-29 10:30:32 +0900

    4

--- HelloProject/src/mabang/MabangMain.java
+++ HelloProject/src/mabang/MabangMain.java
@@ -1,84 +1,48 @@
 package mabang;
 import java.util.*;
 
-
-
-public class MabangMain {
-    static int arr[][] = new int[5][5];
-
-    public void ma(int number)
-    {
-        int hng,h;//행
-        int yul,y;//열
-        int count;//채울숫자
-
-        //0행 가운데 1을 둠
-        hng=0;
-        yul=(number-1)/2;
-        arr[0][yul]=1;
-
-        for(count=2;count<=number*number;count++)
-        {//현재 행이 제일 윗행이면 제일 아래행으로
-            if(hng==0)
-            {
-                h=number-1;
-            }
-            else
-            {
-                h=hng-1;
-            }
-            //현재 열이 가장 왼쪽열이면 제일 오른쪽 열로
-            //아니면 하나 왼쪽 열로 이동
-            if(yul==0)
-            {
-                y=number-1;
-            }
-            else
-            {
-                y=yul-1;
-            }
-            //이동 후의 위치가 빈칸이면
-            if(arr[h][y]==0)
-            {
-                hng=h;
-                yul=y;
-            }
-            else //빈칸 아니면 하나 아래행으로
-            {
-                hng++;
-            }
-            arr[hng][yul]=count;
-        }
-    }
-
+public class MabangMain
+{
     public static void main(String[] args)
     {
-
-       int value=5,sum=0;
-       int i=0,j=0;
-       MabangMain m = new MabangMain();
-       m.ma(value);
-       for(i=0;i<value;i++)
-       {
-           for(j=0;j<value;j++)
-           {
-               if(arr[i][j]<10) System.out.print(" ");
-               else System.out.print(" ");
-           }
-
-       }
-        for(i=0;i<5;i++)// 좌표확인용
+        // S(5,5) : 5행 5열로 된 마방진을 보관하는 2차원배열
+        // R : 현재 처리할 배열 원소의 행
+        // C : 현재 처리할 배열 원소의 열
+        // NR : 다음에 처리할 배열 원소의 행
+        // NC : 다음에 처리할 배열 원소의 열
+        // N : 마방진의 각 원소에 할당될 1부터 25까지의 숫자를 보관
+        int S[][] = new int[3][3];
+        int R=0,C=1;
+        for(int N=1;N<=9;N++)
         {
-            for(j=0;j<5;j++)
+            S[R][C] = N;
+            int NR = R-1;
+            int NC = C+1;
+            if(NR == -1)
             {
-                System.out.print(i+"/"+j+"\t");
-
+                NR = 2;
+            }
+            if(NC == 3)
+            {
+                NC = 0;
+            }
+            if(S[NR][NC]==0)
+            {
+                R = NR;
+                C = NC;
+            }
+            else
+            {
+                R++;
+            }
+        }
+        for(int NR = 0;NR<=2;NR++)
+        {
+            for(int NC=0;NC<=2;NC++)
+            {
+                System.out.print(S[NR][NC]+"\t");
             }
             System.out.println();
         }
-
-
-
-
     }
 }
--- HelloProject/src/rainNumber/RainTest.java
+++ HelloProject/src/rainNumber/RainTest.java
@@ -1,24 +1,24 @@
 package rainNumber;
 
-public class RainTest {
+public class RainTest
+{
     public static void main(String[] args)
     {
         int[][] table = new int[5][5];
         int count=0;
-
-       for(int sum=0;sum<9;sum++) //행과 열의 숫자합이 최대 8이므로 9전까지
-       {
-           for(int j=0;j<5;j++) //행
-           {
-               for(int k=0;k<5;k++) //열
-               {
-                   if(j+k==sum) //행과 열의 숫자가 하나씩 늘어나는데 둘의 합이
-                   {
-                       count++;
-                       table[j][k]=count;
-                   }
-               }
-           }
+        for(int num=0;num<9;num++)
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(i+j==num)
+                    {
+                        count++;
+                        table[i][j]=count;
+                    }
+                }
+            }
         }
         for(int i=0;i<5;i++)
         {
@@ -28,46 +28,5 @@ public class RainTest {
             }
             System.out.println();
         }
-
-
-
-
-
-       int[][] arr = new int[5][5];
-       int value=0;
-
-       for(int sum2=0;sum2<9;sum2++)
-       {
-           for(int i=0;i<5;i++)
-           {
-               for(int j=0;j<5;j++)
-               {
-                   if(i+j==sum2)
-                   {
-                       value++;
-                       arr[i][j]=value;
-                   }
-               }
-           }
-       }
-        for(int i=0;i<5;i++)
-        {
-            for(int j=0;j<5;j++)
-            {
-                System.out.print(arr[i][j]+"\t");
-            }
-            System.out.println();
-        }
-
-
-
-
-
-
-
-
-
-
-
     }
 }
